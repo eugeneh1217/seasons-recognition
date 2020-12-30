2ac3dacd120fa95ba75a6064001ed36165ce1f94
Eugeneh1217 <Eugeneh1217@gmail.com>
2020-12-30 15:42:57 -0500

    fixed assignment bug in InputData lol

--- InputData.py
+++ InputData.py
@@ -9,13 +9,4 @@ class InputData:
     def resize(self, resolution):
         for category in self.data:
             for key in category.keys():
-                category[key].resize(resolution)
-
-
-lst_of_images = InputData(data.data)
-
-lst_of_images.resize((400, 400))
-
-print(lst_of_images.data[0]["cloudy1"].size)
-
-
+                category[key] = category[key].resize(resolution)
